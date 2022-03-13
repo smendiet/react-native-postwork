065e0e45d3d7de8a6c4f27bd9265e064a13093c7
smendiet <smendiet@gmail.com>
2022-03-13 15:53:04 -0500

    Agregado proyecto de practica de sesión 3

--- sesion-03/README.md
+++ sesion-03/README.md
@@ -1,7 +1,16 @@
 # Sesión 3 - Postwork
 
 1. Terminar Challenge de Layout
-2. Prácticar otros layout en [Get Started With Layouts in React Native](https://github.com/tutsplus/get-started-with-react-native-layouts/) del [post](https://code.tutsplus.com/es/tutorials/get-started-with-layouts-in-react-native--cms-27418)
+2. Prácticar otros layout en [Github](https://github.com/tutsplus/get-started-with-react-native-layouts/) del [Get Started With Layouts in React Native](https://code.tutsplus.com/es/tutorials/get-started-with-layouts-in-react-native--cms-27418)
+
+
+![Horizontal Stack Layout](./images/HorizontalStackLayout.png)
+
+![Grid Layout With Space Between](./images/GridLayoutWithSpaceBetween.png)
+
+![Absolute Layout](./images/AbsoluteLayout.png)
+
+
 3. Realizar Postwork de Layout
 4. Realizar componente de Login
 5. Realizar montaje del Backend
---
+++ sesion-03/apps/LayoutApp/App.js
@@ -0,0 +1,35 @@
+import { View, Text, Dimensions } from 'react-native'
+import React from 'react'
+import Home from "./src/screen/container/Home";
+import StyleSheetPreview from './src/css-learn/StyleSheetPreview';
+import RefactorHtml from './src/css-learn/RefactorHtml';
+import Challenge from './src/css-learn/Challenge';
+import ExampleLayout from './src/layout-learn/ExampleLayout';
+import VerticalStackLayout from './src/layout-learn/VerticalStackLayout';
+import Reto from './src/layout-learn/Reto';
+import GridLayout from './src/layout-learn/GridLayout';
+
+import HorizontalStackLayout from "./src/layout-learn/HorizontalStackLayout";
+import GridLayoutWithSpaceBetween from './src/layout-learn/GridLayoutWithSpaceBetween';
+import AbsoluteLayout from "./src/layout-learn/AbsoluteLayout";
+
+export default function App() {
+  return (
+    <View style={{flex: 1}}>
+     {/*
+      <StyleSheetPreview />
+      <RefactorHtml />
+      <Challenge />
+
+
+      <ExampleLayout />
+      <VerticalStackLayout />
+      <Reto />
+
+
+      <GridLayout />
+     */} 
+      <AbsoluteLayout />
+    </View>
+  );
+}
